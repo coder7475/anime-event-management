97271da2721ceebf58fa42149a6904e2c579cdcb
coder7475 <robiulhossain7475@gmail.com>
2023-10-06 23:40:03 +0600

    feat: customize the band logo

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,7 +1,7 @@
 function Brand() {
   return (
     <div className="flex justify-center py-2 bg-charcoal">
-      <img src="/src/assets/images/brandLogo.png" alt="brandlogo" className="w-44 h-44"/>      
+      <img src="/src/assets/images/newLogo.png" alt="brandlogo" className="w-44 h-44"/>      
     </div>
   )
 }
