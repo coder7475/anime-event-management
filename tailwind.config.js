79dc57ff8c5cf0de459af5e1b878f1812670fe00
coder7475 <robiulhossain7475@gmail.com>
2023-10-06 20:20:24 +0600

    feat: set titullium web as a font for display

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display : "'Titillium Web', sans-serif",
+      },
+    },
   },
   plugins: [],
 }
