6d8dc52faaa1e0f781bae4284cffe8ac3d3e128e
coder7475 <robiulhossain7475@gmail.com>
2023-10-07 18:03:59 +0600

    feat: add service card component

---
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,23 @@
+function ServiceCard({ service }) {
+  const { name, image, price, description} = service;
+  return (
+    <div className="card bg-base-100 border-2 justify-center items-center">
+      <figure className="px-10 pt-10">
+        <img
+          src={image}
+          alt="Shoes"
+          className="rounded-xl"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{name}</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ServiceCard;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import ServiceCard from "../ServiceCard/ServiceCard";
 
 function Services() {
   const [services, setServices] = useState([]);
@@ -10,10 +11,13 @@ function Services() {
   }, [])
 
   return (
-    <div >
+    <div className="max-w-5xl mx-auto">
       <h2 className="text-5xl font-bold mt-20 text-center">
         Our Services: {services.length}
       </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        {services.map(service => <ServiceCard key={service.id} service={service}></ServiceCard>)}
+      </div>
     </div>
   )
 }
