7fac7c9bd63cb76f80737ccfab56bfa072d53a01
coder7475 <robiulhossain7475@gmail.com>
2023-10-07 12:03:48 +0600

    feat: add a navbar to main layout

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-charcoal text-white p-2">
+      <ul className="flex flex-col md:flex-row justify-center align-center gap-4 md:gap-20 text-3xl md:text-4xl">
+        <li className="text-center">
+          <NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="text-center">
+          <NavLink
+            to="/services"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+          >
+            Services
+          </NavLink>
+        </li>
+        <li className="text-center">
+          <NavLink
+            to="/events"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+          >
+            Featured Events
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Brand from "../components/Brand/Brand";
+import Navbar from "../components/Navbar/Navbar";
 
 function Root() {
   return (
     <main>
       <Brand/>
+      <Navbar/>
       <Outlet/>
     </main>
   )
