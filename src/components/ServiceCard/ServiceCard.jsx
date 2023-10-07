bfcef5e1ee90350b51512d02f17ea9a2910d545a
coder7475 <robiulhossain7475@gmail.com>
2023-10-07 21:23:11 +0600

    feat: improve services description

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -7,14 +7,14 @@ function ServiceCard({ service }) {
   return (
     <div className="card justify-center items-center shadow-xl ">
       <figure className="px-10 pt-10 w-full">
-        <img src={image} alt="Shoes" className="rounded-xl h-44 w-full" />
+        <img src={image} alt="service" className="rounded-xl h-44 w-full" />
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title text-2xl text-persianGreen font-bold h-16">{name}</h2>
         <h6 className="text-2xl text-saffron">
           <span className="font-semibold text-sandyBrown h-10">Price</span>: {price}
         </h6>
-        <p className="text-xl font-medium h-28">{description}</p>
+        <p className="text-xl font-medium h-28">{description.slice(0, 70)}.....</p>
         <div className="card-actions">
           <button className="btn bg-burntSienna text-white text-lg font-semibold">
             <NavLink to={`/${id}`}>View Details</NavLink>
