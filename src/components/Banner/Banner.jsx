b3ac666a444e345ba80db1283ab35a4256edbcfc
coder7475 <robiulhossain7475@gmail.com>
2023-10-08 18:47:47 +0600

    feat: adjust margin in batter content

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,10 +7,10 @@ function Banner() {
   <div className="hero-overlay bg-opacity-60">
   </div>
   <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
+    <div className="max-w-md flex flex-col gap-36">
       <h1 className="mb-5 text-7xl font-bold">Hello Folks</h1>
-      <p className="mb-5 text-2xl">Anime Events is your premier destination for all things anime, where we specialize in curating unforgettable events that celebrate this dynamic and captivating art form. Let&apos;s dive into world of anime.</p>
-      <NavLink to="/registration">
+      <p className="mt-5 text-2xl">Anime Events is your premier destination for all things anime, where we specialize in curating unforgettable events that celebrate this dynamic and captivating art form. Let&apos;s dive into world of anime.</p>
+      <NavLink to="/registration" >
         <button className="btn bg-burntSienna text-white px-6 text-3xl font-semibold rounded-xl">Register now</button>
       </NavLink>
     </div>
