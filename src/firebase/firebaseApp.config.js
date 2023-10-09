93531dff5171b2b98a6c367e96ec8fddb2b21977
coder7475 <robiulhossain7475@gmail.com>
2023-10-09 17:41:41 +0600

    feat: add .env for auth configuration

--- src/firebase/firebaseApp.config.js
+++ src/firebase/firebaseApp.config.js
@@ -3,15 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+// console.log(import.meta.env.VITE_SOME_KEY) // 123
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBsOyOmO8S1e5MAF_18vmWGW99wJeGEbC8",
-  authDomain: "anime-events-fcd7a.firebaseapp.com",
-  projectId: "anime-events-fcd7a",
-  storageBucket: "anime-events-fcd7a.appspot.com",
-  messagingSenderId: "626317343087",
-  appId: "1:626317343087:web:c7e0c9bb99c841ed68d09a"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
