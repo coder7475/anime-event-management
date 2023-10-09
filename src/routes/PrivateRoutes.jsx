5871b274b9a4745114dc869256325cf5b753d2ab
coder7475 <robiulhossain7475@gmail.com>
2023-10-09 10:29:03 +0600

    feat: improve the loading ui in service details page

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,20 +1,33 @@
 import { useContext } from "react";
 import AuthContext from "../contexts/AuthContext";
 import { Navigate } from "react-router-dom";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 function PrivateRoute({ children }) {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
 
-  if (user) {
-    return children;
+  if (loading)
+    return (
+      <div className="w-full flex justify-center items-center ">
+        <span className="loading loading-ring loading-xs"></span>
+        <span className="loading loading-ring loading-xs"></span>
+        <span className="loading loading-ring loading-sm"></span>
+        <span className="loading loading-ring loading-sm"></span>
+        <span className="loading loading-ring loading-md"></span>
+        <span className="loading loading-ring loading-md"></span>
+        <span className="loading loading-ring loading-lg"></span>
+        <span className="loading loading-ring loading-lg"></span>
+        
+      </div>
+    );
 
-  }
-  return <Navigate to="/login"></Navigate>
+  if (user) return children;
+
+  return <Navigate to="/login"></Navigate>;
 }
 
 PrivateRoute.propTypes = {
   children: PropTypes.object,
-}
+};
 
 export default PrivateRoute;
